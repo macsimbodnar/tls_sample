730fc02a71f57db5f8f5ef97e61c44299df59b2e
Max <max@keyless.io>
2020-12-26 15:35:02 +0100

    Fix the macos build

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
-CXXFLAGS = -Wall -Wno-deprecated -Werror -std=c++17
-LDLIBS = -lssl -lcrypto
+# Get the operating system
+UNAME := $(shell uname)
+
+# Set the compiler flags
+CXXFLAGS += -Wall -Wno-deprecated -Werror -std=c++17
+# Set the linker flags 
+LDLIBS += -lssl -lcrypto
+
+# In case we compile on macos (Darwin is the name) we needs to set the openssl dir
+ifeq ($(UNAME), Darwin)
+LDFLAGS += -L/usr/local/opt/openssl/lib
+CXXFLAGS += -I/usr/local/opt/openssl/include
+endif
 
 all: server client
 
--- server.cpp
+++ server.cpp
@@ -6,18 +6,7 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-#define LOG_I std::cout
-#define END_I std::endl
-
-#define LOG_S LOG_I << "\033[92m"
-#define END_S "\033[37m" << END_I
-
-#define LOG_W LOG_I << "\033[33m"
-#define END_W "\033[37m" << END_I
-
-#define LOG_E LOG_I << "\033[31m"
-#define END_E "\033[37m" << END_I
-
+#include "defines.hpp"      // Contains the log macros and other defines
 
 
 int main(int argc, char **argv) {
@@ -35,7 +24,7 @@ int main(int argc, char **argv) {
         LOG_E << "Failed to init the socket: " << strerror(err) << END_E;
     }
 
-    struct sockaddr_in addr;
+    // struct sockaddr_in addr;
 
 
 
